65676a71d4ad47f1103ea3f2e5977f931eb11158
Daniel Reina <dannyflatiron@gmail.com>
2019-12-11 07:08:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,8 +19,8 @@ class Artist
   end
 
   def add_song(song)
-    # song.artist = self
-    @songs << song
+    song.artist = self
+    # the song belongs to the artist 
   end
 
   # def add_song(song)
